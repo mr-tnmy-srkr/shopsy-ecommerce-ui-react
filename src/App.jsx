2b0fde9a7c6d348104eadb0368a37b286381e2d9
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2024-04-14 22:01:07 +0530

    testimonial added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect, useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Products from "./components/Products/Products";
-import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
 import Subscribe from "./components/Subscribe/Subscribe";
+import TopProducts from "./components/TopProducts/TopProducts";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 const App = () => {
   const [orderPopup, setOrderPopup] = useState(false);
@@ -26,11 +28,13 @@ const App = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar handleOrderPopup={handleOrderPopup} />
-      {/* <Hero handleOrderPopup={handleOrderPopup} /> */}
+      <Hero handleOrderPopup={handleOrderPopup} />
       <Products />
       <TopProducts handleOrderPopup={handleOrderPopup} />
       <Banner />
-      <Subscribe/>
+      <Subscribe />
+      <Products />
+      <Testimonials/>
     </div>
   );
 };
